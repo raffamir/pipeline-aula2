feffe8c762bbaf9a5e5eeb8920640cf757cca0a3
raffamir <raffamir@gmail.com>
2026-06-03 20:03:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,4 @@
 def soma(a, b):
-    return a + b
+    if a < 0 or b < 0:
+        return "Erro: Não são permitidos números negativos"  # Ramo A
+    return a + b                                            # Ramo B
